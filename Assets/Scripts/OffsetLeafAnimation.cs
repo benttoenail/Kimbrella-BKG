13aeffcfbf40ff28d3d3a057c5b42dd2d6725682
benttoenail <maxrose8841@gmail.com>
2017-09-17 16:16:16 +0200

    First Commit

---
+++ Assets/Scripts/OffsetLeafAnimation.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OffsetLeafAnimation : MonoBehaviour {
+
+	List<Transform> leaf = new List<Transform>();
+	private int count;
+
+	public float Slowest = 0.1f;
+	public float Fastest = 0.5f;
+
+	// Use this for initialization
+	void Start () {
+
+		count =  gameObject.transform.childCount;
+
+		for(int i = 0; i < count; i ++){
+
+			Transform temp = gameObject.transform.GetChild(i);
+			leaf.Add(temp);
+		}
+			
+		ScatterAnimations();
+
+	}
+
+	//Play animations at different interval
+	void ScatterAnimations(){
+
+		Animator anim;
+
+		for(int i = 0; i < count; i++){
+			float speed = Random.Range(Slowest, Fastest);
+
+			anim = leaf[i].gameObject.GetComponent<Animator>();
+			anim.Play("Sway", -1, Random.Range(0f, 1.0f));
+			anim.speed = speed;
+		}
+
+	}
+
+}
---
+++ Assets/Scripts/ProjectCaustics.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//[ExecuteInEditMode]
+public class ProjectCaustics : MonoBehaviour {
+
+	public float fps = 15.0f;
+	public Texture2D[] frames;
+
+	private int frameIndex;
+	private Projector projector;
+
+	int count = 31; //Lazy implementation of frame count... 
+
+	//IF frames are not set in the "Frames" array, implement this again with "Execute in Edit Mode" turned on
+
+	/*
+	void Awake(){
+
+		frames = new Texture2D[count];
+
+		for(int i = 0; i < count; i ++){
+			frames[i] = Resources.Load("Caustics/Caustic-ImgSeq_" + i) as Texture2D;
+		}
+
+	}
+*/
+
+	void Start(){
+
+		projector = GetComponent<Projector>();
+		NextFrame();
+		InvokeRepeating("NextFrame", 1 / fps, 1 / fps);
+
+	}
+
+	void NextFrame(){
+		projector.material.SetTexture("_ShadowTex", frames[frameIndex]);
+		frameIndex = (frameIndex + 1) % frames.Length;
+	}
+
+}
